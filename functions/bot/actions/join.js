8548ae3bfa25e0d02617bc90533e318bde1ae4f7
Mark Danovich <mark.danovich@gmail.com>
2023-03-05 21:32:15 +0000

    added join, changed start

---
+++ functions/bot/actions/join.js
@@ -0,0 +1,37 @@
+const { newUser } = require("../components/fauna");
+const { getUser } = require("../components/helper");
+
+module.exports = async (ctx) => {
+  const { id, isBot, name, username } = getUser(ctx.from);
+
+  if (isBot) {
+    return ctx.reply(`Sorry I only interact with humans!`);
+  }
+
+  try {
+    let isNewUser = await newUser(id, name, username);
+    console.log(isNewUser);
+    if (isNewUser) {
+      //   ctx.telegram.sendMessage(
+      //     (chat_id = 396532677),
+      //     (text = "hello from ltc")
+      //   );
+      //   ctx.telegram.sendMessage(
+      //     (chat_id = 396532677),
+      //     (text =
+      //       "This week you are meeting @Massiania! say Hi to Mark and schedule your meeting :)")
+      //   );
+      return ctx.reply(
+        `Thank you for joining LondonTechConnect! You have been added to our weekly pairings list, and we'll be in touch soon with details on your coffee match.`
+      );
+    } else {
+      return ctx.reply(
+        `It looks like you're already on our weekly pairings list! We're so glad to have you as part of our community, 
+        and we hope you're enjoying meeting new people through LondonTechConnect.\n
+        If you need to pause the pairings for any reason, use the /pause command. If you'd like to remove yourself from the pairings, type /remove. And if you want to start receiving pairings again, use the /resume command.`
+      );
+    }
+  } catch (e) {
+    return ctx.reply(`Error occured`);
+  }
+};
--- functions/bot/actions/start.js
+++ functions/bot/actions/start.js
@@ -8,26 +8,7 @@ module.exports = async (ctx) => {
     return ctx.reply(`Sorry I only interact with humans!`);
   }
 
-  try {
-    let isNewUser = await newUser(id, name, username);
-    console.log(isNewUser);
-    if (isNewUser) {
-      ctx.telegram.sendMessage(
-        (chat_id = 396532677),
-        (text = "hello from ltc")
-      );
-      ctx.telegram.sendMessage(
-        (chat_id = 396532677),
-        (text =
-          "This week you are meeting @Massiania! say Hi to Mark and schedule your meeting :)")
-      );
-      return ctx.reply(
-        `Hi! you've been added to London Tech Connect, you'll get a notification when we connect you with another member of the group!`
-      );
-    } else {
-      return ctx.reply(`You are already a part of London Tech Connect`);
-    }
-  } catch (e) {
-    return ctx.reply(`Error occured`);
-  }
+  ctx.reply(
+    "Welcome to LondonTechConnect U0001F44B U0001F1FA U0001F1EC, the bot that helps you meet new people over a cup of coffee! U0002615 \n Whether you're new to London or just looking to expand your network, our bot will match you with a random person in the chat for a friendly chat and a hot drink. \n \n To join our weekly pairings, simply type /join in the chat. \n If you need to pause the pairings for any reason, use the /pause command. \n If you'd like to remove yourself from the pairings, type /remove. \n And if you want to start receiving pairings again, use the /resume command. \n\n\n Remember, meeting new people can be a great way to learn new things, expand your horizons, and have fun! \n So don't be shy, join LondonTechConnect today and start connecting with your fellow Londoners. "
+  );
 };
