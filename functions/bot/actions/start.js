d696c464a6e9e0dcc4f50bc934c8128846404f42
Mark Danovich <mark.danovich@gmail.com>
2023-03-05 20:55:02 +0000

    added send message

--- functions/bot/actions/start.js
+++ functions/bot/actions/start.js
@@ -12,6 +12,10 @@ module.exports = async (ctx) => {
     let isNewUser = await newUser(id, name);
     console.log(isNewUser);
     if (isNewUser) {
+      ctx.telegram.sendMessage(
+        (chat_id = 258865258),
+        (text = "hello from ltc")
+      );
       return ctx.reply(
         `Hi! you've been added to London Tech Connect, you'll get a notification when we connect you with another member of the group!`
       );
