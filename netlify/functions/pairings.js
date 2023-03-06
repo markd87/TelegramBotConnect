603d023faaf370315f57758cd47f3ad326890080
Mark Danovich <mark.danovich@gmail.com>
2023-03-06 23:42:01 +0000

    added schedule

---
+++ netlify/functions/pairings.js
@@ -0,0 +1,39 @@
+const faunadb = require("faunadb");
+
+const client = new faunadb.Client({ secret: process.env.FAUNA_SECRET_KEY });
+const q = faunadb.query;
+
+function shuffleArray(array) {
+  // in place shuffling of array
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+}
+
+shuffleArray(arr);
+
+exports.handler = async function (event, context) {
+  console.log("Received event:", event);
+
+  let all_paricipants = await client.query(
+    Map(
+      Paginate(Match(Index("participants"), true)),
+      Lambda("user", {
+        userId: Select(["data", "userId"], Get(Var("user"))),
+      })
+    )
+  );
+
+  console.log(all_paricipants);
+
+  let last = null;
+  if (all_paricipants.length % 2 != 0) {
+    last = all_paricipants[all_paricipants.length - 1];
+    all_paricipants = all_paricipants.slice(0, all_paricipants.length - 1);
+  }
+
+  return {
+    statusCode: 200,
+  };
+};
