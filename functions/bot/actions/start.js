1cb76b7de61cab046830e332ef163219820a6e88
Mark Danovich <mark.danovich@gmail.com>
2023-03-08 00:10:14 +0000

    changed to every day at 8 30

--- functions/bot/actions/start.js
+++ functions/bot/actions/start.js
@@ -3,13 +3,23 @@ const { getUser } = require("../components/helper");
 module.exports = async (ctx) => {
   const { id, isBot, name, username } = getUser(ctx.from);
 
-  const isMember = await ctx.telegram.getChatMember(
+  const isMember_test = await ctx.telegram.getChatMember(
     (chat_id = -997370885),
     (uer_id = id)
   );
+
+  const isMember = await ctx.telegram.getChatMember(
+    (chat_id = -1530060246),
+    (uer_id = id)
+  );
+
   console.log(isMember.status);
 
-  if (isMember.status != "member") {
+  if (
+    isMember.status != "member" &&
+    isMember.status != "creator" &&
+    isMember.status != "administrator"
+  ) {
     return ctx.reply(`Sorry, this bot is only for the LondonTechCommunity`);
   }
 
--- functions/pairings/pairings.js
+++ functions/pairings/pairings.js
@@ -152,11 +152,11 @@ exports.handler = async function (event, context) {
       } else {
         await bot.telegram.sendMessage(
           (chat_id = user_1),
-          (text = `You've been randomly matched with @${pairs[i][1].username} for a coffee meetup. \nI hope you both have a great time getting to know each other over a cup of coffee. \nFeel free to coordinate a time and location that works for both of you. Enjoy!`)
+          (text = `Hello! You've been randomly matched with @${pairs[i][1].username} for a coffee meetup. \nI hope you both have a great time getting to know each other over a cup of coffee. \nFeel free to coordinate a time and location that works for both of you. Enjoy!`)
         );
         await bot.telegram.sendMessage(
           (chat_id = user_2),
-          (text = `You've been randomly matched with @${pairs[i][0].username} for a coffee meetup. \nI hope you both have a great time getting to know each other over a cup of coffee. \nFeel free to coordinate a time and location that works for both of you. Enjoy!`)
+          (text = `Hello! You've been randomly matched with @${pairs[i][0].username} for a coffee meetup. \nI hope you both have a great time getting to know each other over a cup of coffee. \nFeel free to coordinate a time and location that works for both of you. Enjoy!`)
         );
       }
     }
