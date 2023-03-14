a56593e3e6be05325f3cb4c67ab93d1fa2c2df5e
Mark Danovich <mark.danovich@gmail.com>
2023-03-14 18:29:20 +0000

    test

--- functions/bot/actions/join.js
+++ functions/bot/actions/join.js
@@ -29,7 +29,7 @@ module.exports = async (ctx) => {
         `Welcome! Thank you for joining LondonTechCoffee.\nBefore we match you with someone for a random coffee in our weekly pairings, could you please answer a few quick questions:\n\n`
       );
       //   let name = await
-      ctx.scene.enter("super-wizard");
+      await ctx.scene.enter("super-wizard");
       //   let occupation = await ctx.reply("What is your occupation?");
       //   console.log(name);
       //   console.log(occupation);
