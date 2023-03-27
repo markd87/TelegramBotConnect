ad47494ed8b53394982a992e04c80646d0db28f0
Mark Danovich <mark.danovich@gmail.com>
2023-03-27 14:25:37 -0500

    add clarification to questions

--- functions/bot/bot.js
+++ functions/bot/bot.js
@@ -33,7 +33,7 @@ const superWizard = new Scenes.WizardScene(
     }
 
     await ctx.reply(
-      "What is your Instagram username?",
+      "What is your Instagram username (please only provide the username)?",
       Markup.inlineKeyboard([Markup.button.callback("Skip", "skip")])
     );
     return ctx.wizard.next();
@@ -48,7 +48,7 @@ const superWizard = new Scenes.WizardScene(
     }
 
     await ctx.reply(
-      "What is your linkedin profile link?",
+      "What is your linkedin profile link (please provide a full link)?",
       Markup.inlineKeyboard([Markup.button.callback("Skip", "skip")])
     );
     return ctx.wizard.next();
