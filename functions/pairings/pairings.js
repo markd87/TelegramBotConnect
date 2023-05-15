61846227de4a9e77dca40bb5dba755ee059a9524
Mark Danovich <mark.danovich@gmail.com>
2023-05-15 10:42:49 +0100

    Update pairings.js
    fixed create_pair background function url

--- functions/pairings/pairings.js
+++ functions/pairings/pairings.js
@@ -4,7 +4,7 @@ exports.handler = async function (event, context) {
   console.log("Received event:", event);
 
   let url =
-    "https://aquamarine-horse-60f550.netlify.app/.netlify/create_pairs-background";
+    "https://aquamarine-horse-60f550.netlify.app/.netlify/functions/create_pairs-background";
 
   console.log("Fetching background function");
   let result = await axios.post(url, {
