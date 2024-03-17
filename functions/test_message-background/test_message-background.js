6898794fbfaa163f601367b1db3c2f29795b7d76
Mark Danovich <mark.danovich@gmail.com>
2024-03-17 20:17:09 +0000

    test getting username

--- functions/test_message-background/test_message-background.js
+++ functions/test_message-background/test_message-background.js
@@ -4,9 +4,13 @@ const bot = new Telegraf(process.env.TELEGRAM_BOT_TOKEN);
 
 exports.handler = async function (event, context) {
   try {
+    const chat_id = 258865258;
+
+    const chat = await bot.telegram.getChat(chat_id);
+
     await bot.telegram.sendMessage(
-      (chat_id = 258865258),
-      (text = `Test background`)
+      (chat_id = chat_id),
+      (text = `username test: ${chat.username}`)
     );
   } catch (error) {
     console.log(error);
